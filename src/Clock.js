87a6fd8e9113e269323d1d302296dfd5cc64b818
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-04-22 16:20:46 +0200

    format fix

--- src/Clock.js
+++ src/Clock.js
@@ -3,14 +3,14 @@ import React, { useState, useEffect } from 'react';
 function Clock() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-useEffect(() => {
-  const interval = setInterval(() => {
-    setTime(new Date().toLocaleTimeString());
-  }, 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
 
-  // Clean up the interval when the component unmounts
-  return () => clearInterval(interval);
-}, []);
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <span>{time}</span>
